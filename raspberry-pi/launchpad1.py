d4d74cd246c75463212a651a4860e16bcfef07c6
dhalber11 <dhalber11@charlottesvilleschools.org>
2023-09-08 10:22:34 -0400

    final launchpad2 and commenting

--- raspberry-pi/launchpad1.py
+++ raspberry-pi/launchpad1.py
@@ -5,14 +5,14 @@ led = digitalio.DigitalInOut(board.GP0)
 led.direction = digitalio.Direction.OUTPUT
 import time 
 
-Seconds = 10 
+Seconds = 10  #Define the variable to count down from
 
 while True:
-    if Seconds > 0:
-        print("T-Minus",Seconds)
-        time.sleep(0.5)
-        Seconds = Seconds -1
-    else
-    print("LIFTOFF")
-    time.sleep(.5)
+    if Seconds > 0:    #if the countdown has not already hit zero then do this
+        print("T-Minus",Seconds)  #print the countdown
+        time.sleep(0.5)     #pause 1/2 second
+        Seconds = Seconds -1  #actually reduce the variable by 1
+    else: 
+        print("LIFTOFF")  #when seconds is less than 0 print LIFTOFF
+        time.sleep(.5)
         
--- raspberry-pi/launchpad2.py
+++ raspberry-pi/launchpad2.py
@@ -7,15 +7,18 @@ Rled = digitalio.DigitalInOut(board.GP1)
 Rled.direction = digitalio.Direction.OUTPUT
 import time 
 
+Seconds = 10    #Define the variable 
+
 while True:
     if Seconds > 0:
-        print("T-Minus",Seconds)
+        print("T-Minus",Seconds)  #Print the countdown
         time.sleep(0.5)
-        Seconds = Seconds -1
-        Rled.value = True
-        time.sleep(.5)
-        Rled.value = False
+        Seconds = Seconds -1    #reduce the variable by 1 each time
+        Rled.value = True    #turn the red led on
+        time.sleep(.5)       #pause 1/2 second
+        Rled.value = False  #turn the red led off, blinking it
     else:
-        print("LIFTOFF")
+        print("LIFTOFF")    #print LIFTOFF when variable is 0
+        Gled.value = True   #turn the green led on to indicate liftoff
         time.sleep(.5)
         
