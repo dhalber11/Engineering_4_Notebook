75ab17b3e08e4de9f85868b662aea9243eae98db
dhalber11 <dhalber11@charlottesvilleschools.org>
2023-09-11 10:46:15 -0400

    started launchpad3

---
+++ raspberry-pi/launchpad3.py
@@ -0,0 +1,31 @@
+#type: ignore
+import board
+import digitalio
+Gled = digitalio.DigitalInOut(board.GP0)
+Gled.direction = digitalio.Direction.OUTPUT
+Rled = digitalio.DigitalInOut(board.GP1)
+Rled.direction = digitalio.Direction.OUTPUT
+button = digitalio.DigitalInOut(board.GP15)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP 
+import time 
+
+Seconds = 10    #Define the variable 
+Bpress = True 
+
+while True:
+    if Bpress == False:
+        Seconds =10
+        Bpress = False
+        if Seconds > 0:
+            print("T-Minus",Seconds)  #Print the countdown
+            time.sleep(0.5)
+            Seconds = Seconds -1    #reduce the variable by 1 each time
+            Rled.value = True    #turn the red led on
+            time.sleep(.5)       #pause 1/2 second
+            Rled.value = False  #turn the red led off, blinking it
+        else:
+            print("LIFTOFF")    #print LIFTOFF when variable is 0
+            Gled.value = True   #turn the green led on to indicate liftoff
+            time.sleep(.5)
+            Bpress = True
