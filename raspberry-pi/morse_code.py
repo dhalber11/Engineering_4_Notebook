9c376c77e3cac61bddaf6f901f40fbdc434fddaf
dhalber11 <dhalber11@charlottesvilleschools.org>
2023-11-01 10:30:04 -0400

    morse code

---
+++ raspberry-pi/morse_code.py
@@ -0,0 +1,30 @@
+#type: ignore
+import board
+
+# Dictionary representing the morse code chart
+MORSE_CODE = { 'A':'.-', 'B':'-...',
+    'C':'-.-.', 'D':'-..', 'E':'.',
+    'F':'..-.', 'G':'--.', 'H':'....',
+    'I':'..', 'J':'.---', 'K':'-.-',
+    'L':'.-..', 'M':'--', 'N':'-.',
+    'O':'---', 'P':'.--.', 'Q':'--.-',
+    'R':'.-.', 'S':'...', 'T':'-',
+    'U':'..-', 'V':'...-', 'W':'.--',
+    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+    '1':'.----', '2':'..---', '3':'...--',
+    '4':'....-', '5':'.....', '6':'-....',
+    '7':'--...', '8':'---..', '9':'----.',
+    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+    '?':'..--..', '/':'-..-.', '-':'-....-',
+    '(':'-.--.', ')':'-.--.-', ' ':'/'}
+
+while True: 
+    #print("Enter your Morse Code Message, or press -q to quit")
+    Text = input("Enter your Morse Code Message, or press -q to quit:" )
+    if Text == "-q":
+        ("Message ended")
+        break
+    Up_text = Text.upper()
+    for letter in Up_text: 
+        print(MORSE_CODE [f"{letter}"], end=" ")
+        
